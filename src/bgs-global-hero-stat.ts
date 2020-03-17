900b10cdacc4b5154c9b8833ac476013e007f1c9
sebastientromp <sebastien.tromp@gmail.com>
2020-03-17 10:19:27 +0100

    also load tribe stats and warband delta stats

--- src/bgs-global-hero-stat.ts
+++ src/bgs-global-hero-stat.ts
@@ -2,4 +2,6 @@ export class BgsGlobalHeroStat {
 	id: string;
 	popularity: number;
 	averagePosition: number;
+	tribesStat: readonly { tribe: string; percent: number }[];
+	warbandStats: readonly { turn: number; totalStats: number }[];
 }
--- src/retrieve-bgs-global-stats.ts
+++ src/retrieve-bgs-global-stats.ts
@@ -8,24 +8,92 @@ import { getConnection } from './db/rds';
 export default async (event): Promise<any> => {
 	try {
 		const mysql = await getConnection();
-		const query = `
+
+		// Global stats, like popularity, etc.
+		const heroStatsQuery = `
 			SELECT * FROM bgs_hero_stats 
 			WHERE date is NULL
 			ORDER BY heroCardId ASC
 		`;
-		console.log('prepared query', query);
-		const dbResults: readonly any[] = await mysql.query(query);
-		console.log('executed query', dbResults && dbResults.length, dbResults && dbResults.length > 0 && dbResults[0]);
+		console.log('prepared query', heroStatsQuery);
+		const heroStatsDbResults: readonly any[] = await mysql.query(heroStatsQuery);
+		console.log(
+			'executed query',
+			heroStatsDbResults && heroStatsDbResults.length,
+			heroStatsDbResults && heroStatsDbResults.length > 0 && heroStatsDbResults[0],
+		);
+		const heroStats = heroStatsDbResults.map(
+			result =>
+				({
+					id: result.heroCardId,
+					averagePosition: result.averagePosition,
+					popularity: result.popularity,
+				} as BgsGlobalHeroStat),
+		);
+
+		// Tribes stats
+		const tribeDateQuery = `
+			SELECT creationDate FROM bgs_hero_tribes_at_end ORDER BY id desc limit 1
+		`;
+		console.log('trbe date', await mysql.query(tribeDateQuery));
+		const tribeLastDate: Date = (await mysql.query(tribeDateQuery))[0].creationDate;
+		const tribesAtEndStatsQuery = `
+			SELECT * FROM bgs_hero_tribes_at_end 
+			WHERE creationDate = '${tribeLastDate.toISOString()}'
+			ORDER BY heroCardId ASC
+		`;
+		console.log('prepared query', tribesAtEndStatsQuery);
+		const tribesDbResults: readonly any[] = await mysql.query(tribesAtEndStatsQuery);
+		console.log(
+			'executed query',
+			tribesDbResults && tribesDbResults.length,
+			tribesDbResults && tribesDbResults.length > 0 && tribesDbResults[0],
+		);
+		const heroStatsWithTribes = heroStats.map(stat => {
+			const relevantTribes = tribesDbResults.filter(tribeStat => tribeStat.heroCardId === stat.id);
+			return {
+				...stat,
+				tribesStat: relevantTribes.map(tribe => ({
+					tribe: tribe.tribe.toLowerCase(),
+					percent: tribe.percent,
+				})),
+			} as BgsGlobalHeroStat;
+		});
+		console.log('hero stats with tribes', heroStatsWithTribes);
+
+		// Warband stats stats
+		const warbandDateQuery = `
+			SELECT creationDate FROM bgs_hero_warband_stats ORDER BY id desc limit 1
+		`;
+		console.log('warband date', (await mysql.query(warbandDateQuery))[0].creationDate);
+		const warbandLastDate: Date = (await mysql.query(warbandDateQuery))[0].creationDate;
+		console.log('date', warbandLastDate, warbandLastDate.toISOString(), '' + warbandLastDate);
+		const warbandStatsQuery = `
+			SELECT * FROM bgs_hero_warband_stats 
+			WHERE creationDate = '${warbandLastDate.toISOString()}'
+			ORDER BY heroCardId, turn ASC
+		`;
+		console.log('prepared query', warbandStatsQuery);
+		const warbandDbResults: readonly any[] = await mysql.query(warbandStatsQuery);
+		console.log(
+			'executed query',
+			warbandDbResults && warbandDbResults.length,
+			warbandDbResults && warbandDbResults.length > 0 && warbandDbResults[0],
+		);
+		const heroStatsWithWarband = heroStatsWithTribes.map(stat => {
+			const relevantInfo = warbandDbResults.filter(warbandStat => warbandStat.heroCardId === stat.id);
+			return {
+				...stat,
+				warbandStats: relevantInfo.map(info => ({
+					turn: info.turn,
+					totalStats: info.statsDelta,
+				})),
+			} as BgsGlobalHeroStat;
+		});
+		console.log('hero stats with warbnd stats', heroStatsWithWarband);
 
 		const result = {
-			heroStats: dbResults.map(
-				result =>
-					({
-						id: result.heroCardId,
-						averagePosition: result.averagePosition,
-						popularity: result.popularity,
-					} as BgsGlobalHeroStat),
-			),
+			heroStats: heroStatsWithWarband,
 		} as BgsGlobalStats;
 
 		const response = {
