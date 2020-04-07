fdef187addba542e2f901094c97dc5e29b0cafaf
sebastientromp <sebastien.tromp@gmail.com>
2020-04-07 14:28:57 +0200

    BUG: fix issue where new heroes didn't have stats

--- src/retrieve-bgs-global-stats.ts
+++ src/retrieve-bgs-global-stats.ts
@@ -9,19 +9,37 @@ export default async (event): Promise<any> => {
 	try {
 		const mysql = await getConnection();
 
+		// First get the list of active heroes
+		const heroesDateQuery = `
+			SELECT date FROM bgs_hero_stats ORDER BY id desc limit 1
+		`;
+		console.log('heroesDateQuery', await mysql.query(heroesDateQuery));
+		const heroesLastDate: Date = (await mysql.query(heroesDateQuery))[0].date;
+		console.log('heroesLastDate', heroesLastDate);
+		const allHeroesQuery = `
+			SELECT heroCardId FROM bgs_hero_stats 
+			WHERE date = '${heroesLastDate.toISOString()}'
+			ORDER BY heroCardId ASC
+		`;
+		const allHeroesDbResult: readonly any[] = await mysql.query(allHeroesQuery);
+		console.log('allHeroesDbResult', allHeroesDbResult);
+		const allHeroes: string = allHeroesDbResult.map(result => `'${result.heroCardId}'`).join(',');
+		console.log('allHeroes', allHeroes);
+
 		// Global stats, like popularity, etc.
 		const heroStatsQuery = `
 			SELECT * FROM bgs_hero_stats 
 			WHERE date is NULL
+			AND heroCardId in (${allHeroes})
 			ORDER BY heroCardId ASC
 		`;
-		console.log('prepared query', heroStatsQuery);
+		// console.log('prepared query', heroStatsQuery);
 		const heroStatsDbResults: readonly any[] = await mysql.query(heroStatsQuery);
-		console.log(
-			'executed query',
-			heroStatsDbResults && heroStatsDbResults.length,
-			heroStatsDbResults && heroStatsDbResults.length > 0 && heroStatsDbResults[0],
-		);
+		// console.log(
+		// 	'executed query',
+		// 	heroStatsDbResults && heroStatsDbResults.length,
+		// 	heroStatsDbResults && heroStatsDbResults.length > 0 && heroStatsDbResults[0],
+		// );
 		const heroStats = [
 			...heroStatsDbResults.map(
 				result =>
@@ -43,20 +61,20 @@ export default async (event): Promise<any> => {
 		const tribeDateQuery = `
 			SELECT creationDate FROM bgs_hero_tribes_at_end ORDER BY id desc limit 1
 		`;
-		console.log('trbe date', await mysql.query(tribeDateQuery));
+		// console.log('trbe date', await mysql.query(tribeDateQuery));
 		const tribeLastDate: Date = (await mysql.query(tribeDateQuery))[0].creationDate;
 		const tribesAtEndStatsQuery = `
 			SELECT * FROM bgs_hero_tribes_at_end 
 			WHERE creationDate = '${tribeLastDate.toISOString()}'
 			ORDER BY heroCardId ASC
 		`;
-		console.log('prepared query', tribesAtEndStatsQuery);
+		// console.log('prepared query', tribesAtEndStatsQuery);
 		const tribesDbResults: readonly any[] = await mysql.query(tribesAtEndStatsQuery);
-		console.log(
-			'executed query',
-			tribesDbResults && tribesDbResults.length,
-			tribesDbResults && tribesDbResults.length > 0 && tribesDbResults[0],
-		);
+		// console.log(
+		// 	'executed query',
+		// 	tribesDbResults && tribesDbResults.length,
+		// 	tribesDbResults && tribesDbResults.length > 0 && tribesDbResults[0],
+		// );
 		const heroStatsWithTribes = heroStats.map(stat => {
 			const relevantTribes = tribesDbResults.filter(tribeStat => tribeStat.heroCardId === stat.id);
 			return {
@@ -67,27 +85,27 @@ export default async (event): Promise<any> => {
 				})),
 			} as BgsGlobalHeroStat;
 		});
-		console.log('hero stats with tribes', heroStatsWithTribes);
+		// console.log('hero stats with tribes', heroStatsWithTribes);
 
 		// Warband stats stats
 		const warbandDateQuery = `
 			SELECT creationDate FROM bgs_hero_warband_stats ORDER BY id desc limit 1
 		`;
-		console.log('warband date', (await mysql.query(warbandDateQuery))[0].creationDate);
+		// console.log('warband date', (await mysql.query(warbandDateQuery))[0].creationDate);
 		const warbandLastDate: Date = (await mysql.query(warbandDateQuery))[0].creationDate;
-		console.log('date', warbandLastDate, warbandLastDate.toISOString(), '' + warbandLastDate);
+		// console.log('date', warbandLastDate, warbandLastDate.toISOString(), '' + warbandLastDate);
 		const warbandStatsQuery = `
 			SELECT * FROM bgs_hero_warband_stats 
 			WHERE creationDate = '${warbandLastDate.toISOString()}'
 			ORDER BY heroCardId, turn ASC
 		`;
-		console.log('prepared query', warbandStatsQuery);
+		// console.log('prepared query', warbandStatsQuery);
 		const warbandDbResults: readonly any[] = await mysql.query(warbandStatsQuery);
-		console.log(
-			'executed query',
-			warbandDbResults && warbandDbResults.length,
-			warbandDbResults && warbandDbResults.length > 0 && warbandDbResults[0],
-		);
+		// console.log(
+		// 	'executed query',
+		// 	warbandDbResults && warbandDbResults.length,
+		// 	warbandDbResults && warbandDbResults.length > 0 && warbandDbResults[0],
+		// );
 		const heroStatsWithWarband = heroStatsWithTribes.map(stat => {
 			const relevantInfo = warbandDbResults.filter(warbandStat => warbandStat.heroCardId === stat.id);
 			return {
@@ -98,7 +116,7 @@ export default async (event): Promise<any> => {
 				})),
 			} as BgsGlobalHeroStat;
 		});
-		console.log('hero stats with warbnd stats', heroStatsWithWarband);
+		// console.log('hero stats with warbnd stats', heroStatsWithWarband);
 
 		const result = {
 			heroStats: heroStatsWithWarband,
@@ -109,7 +127,7 @@ export default async (event): Promise<any> => {
 			isBase64Encoded: false,
 			body: JSON.stringify({ result }),
 		};
-		console.log('sending back success reponse');
+		// console.log('sending back success reponse');
 		return response;
 	} catch (e) {
 		console.error('issue retrieving stats', e);
